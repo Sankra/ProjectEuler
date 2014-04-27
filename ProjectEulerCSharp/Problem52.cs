b7943277260a6a9598263cd83a555a35e65acf17
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2014-04-27 23:33:46 +0200

    Problem52

---
+++ ProjectEulerCSharp/Problem52.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace ProjectEulerCSharp {
+//	It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+//
+//	Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+	public class Problem52 {
+		public Problem52() {
+		}
+
+		public int FindSmallestIntegerWhereMultiplesContainsSameDigits() {
+			int i = 1;
+			while (true) {
+				if (ContainsSameDigits(i)) {
+					return i;
+				}
+
+				++i;
+			}
+		}
+
+		private bool ContainsSameDigits(int number) {
+			var s = number.ToString();
+			var digits = GetDigits(s);
+			for(int i = 2; i < 7; i++) {
+				var newNumber = number * i;
+				var s2 = newNumber.ToString();
+				if (digits.Length != s2.Length) {
+					return false;
+				}
+
+				var digits2 = GetDigits(s2);
+				for (int j = 0; j < digits.Length; j++) {
+					if (!digits.Contains(digits2[j])) {
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		private static int[] GetDigits(string s) {
+			var digits = new int[s.Length];
+			for(int i = 0; i < digits.Length; i++) {
+				digits[i] = int.Parse(s[i].ToString());
+			}
+			return digits;
+		}
+	}
+}
+
---
+++ ProjectEulerCSharp/Problem97.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace ProjectEulerCSharp {
+	public class Problem97 {
+//		The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 26972593−1; it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the form 2p−1, have been found which contain more digits.
+//
+//		However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433×27830457+1.
+//
+//		Find the last ten digits of this prime number.
+		public Problem97() {
+		}
+
+		public string GetTenLastDigits() {
+			var first = new BigInteger(28433);
+			var second = BigInteger.Pow(2, 7830457);
+			var number = first * second + new BigInteger(1D);
+			var numberAsString = number.ToString();
+			 
+
+			return numberAsString;
+		}
+	}
+}
+
